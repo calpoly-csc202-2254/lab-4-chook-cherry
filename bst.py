d378a336a5c474a7ac2f01e3a8f4cb21cbd9ef32
Seth Cherry <sethcherry@mac.lan>
2025-05-23 13:02:44 -0700

    original code adds

--- bst.py
+++ bst.py
@@ -4,6 +4,54 @@ from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
 
+def main():
+    return
 
-def example_fun(x : int) -> bool:
-    return x < 142
+
+BinTree : TypeAlias = Union[None, "Node"]
+
+@dataclass(frozen=True)
+class Node :
+    val : Any
+    left : BinTree
+    right : BinTree
+
+
+# Examples:
+bt1 : BinTree = Node(0, Node(52, None, None), Node(6, Node(23, None, None), None))
+bt2 : BinTree = None
+
+@dataclass(frozen=True)
+class BinarySearchTree:
+    def comes_before(self, x : Any, y : Any) -> bool:
+        if x > y:
+            return False
+        elif x < y:
+            return True
+        else:
+            return False
+    tree : BinTree
+
+def is_empty(bst : BinarySearchTree) -> bool:
+   match bst.tree:
+         case None:
+              return True
+         case Node(v, l, r):
+              return False
+       
+
+def insert(bst: BinarySearchTree, x: Any) -> BinarySearchTree:
+    def insert_node(node: Optional[Node], x: Any) -> Node:
+        if node is None:
+            return Node(x, None, None)
+        if bst.comes_before(x, node.val):
+            return Node(node.val, insert_node(node.left, x), node.right)
+        else:
+            return Node(node.val, node.left, insert_node(node.right, x))
+
+    return BinarySearchTree(insert_node(bst.tree, x))
+
+            
+
+       
+
--- bst_tests.py
+++ bst_tests.py
@@ -6,10 +6,26 @@ sys.setrecursionlimit(10**6)
 
 from bst import *
 
+# Examples:
+bt1 : BinTree = Node(0, Node(52, None, None), Node(6, Node(23, None, None), None))
+bt2 : BinTree = None
+bst1 : BinarySearchTree = BinarySearchTree(bt1)
+bst2 : BinarySearchTree = BinarySearchTree(bt2)
+
+bt3 : BinTree = Node(13, Node(9, Node(4, None, None), Node(11, None, None)), Node(20, Node(15, None, None), Node(25, None, None)))
+bt4 : BinTree = Node(13, Node(9, Node(4, None, Node(5, None, None)), Node(11, None, None)), Node(20, Node(15, None, None), Node(25, None, None)))
+bst3 : BinarySearchTree = BinarySearchTree(bt3)
+bst4 : BinarySearchTree = BinarySearchTree(bt4)
+bst5 : BinarySearchTree = BinarySearchTree(Node(31, None, None))
+
 class BSTTests(unittest.TestCase):
-    def test_example_fun(self):
-        self.assertEqual(True, example_fun(34))
-        self.assertEqual(False,example_fun(1423))
+    def test_is_empty(self):
+        self.assertEqual(True, is_empty(bst2))
+        self.assertEqual(False, is_empty(bst1))
+
+    def test_insert(self):
+        self.assertEqual(bst4, insert(bst3, 5))
+        self.assertEqual(bst5, insert(bst2, 31))
 
 if (__name__ == '__main__'):
     unittest.main()
